b2689b69a112522b6911dd30be1558d49c2cfe5c
upgle <admin@upgle.com>
2016-07-13 18:16:24 +0700

    LoginKunnect.js 리펙토링.

--- app/components/LoginKunnect.js
+++ app/components/LoginKunnect.js
@@ -18,20 +18,18 @@ import {
 } from 'react-native-material-kit';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { TimeConverter, YoilConverter } from '../util/Kunnect';
-import { saveAppData } from '../actions/appActions';
-
 export default class LoginKunnect extends Component {
 
   constructor(props) {
     super(props);
+
+    console.log(props);
 
     this.state = {
       id: '',
       password: '',
       isLoading: false,
     };
-    this._onPressClose = this._onPressClose.bind(this);
   }
 
   componentWillMount() {
@@ -42,22 +40,7 @@ export default class LoginKunnect extends Component {
     StatusBar.setHidden(false, 'none');
   }
 
-  componentDidMount() {
-    this.props.afterMount();
-  }
-
-  _onPressClose() {
-    this.props.navigator.pop();
-  }
-
   onClickLogin() {
-
-    const { actions } = this.props;
-
-    var data = new FormData();
-    data.append('id', this.state.id);
-    data.append('password', this.state.password);
-
     if(this.state.id.length === 0) {
         Alert.alert(
           '경고',
@@ -74,88 +57,7 @@ export default class LoginKunnect extends Component {
         );
         return;
     }
-
-    this.setState({
-      isLoading: true
-    });
-
-    var p1 = fetch('https://www.kunnect.net/login', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        'Accept': 'application/json',
-        'Cookie': '',
-      },
-      body: data
-    })
-    .then((response) => response.text())
-    .then((responseText) => {
-
-      var response = JSON.parse(responseText);
-
-      console.log(response);
-
-      if(response.result == 200) {
-
-        var courses = {};
-        var times = [];
-
-        fetch('https://www.kunnect.net/timetable/1/mycourse')
-        .then((response) => response.text())
-        .then((responseText) => {
-          var result = JSON.parse(responseText);
-
-          var time;
-          actions.removeAllCourses();
-
-          result.returns.forEach((data) => {
-            time = TimeConverter(data.timeStart, data.timeEnd);
-
-            courses[data.sbjtId] = {
-              id : data.sbjtId,
-              subject: data.subject,
-              professor : data.professor,
-              classroom : data.building + data.classroom
-            };
-            times = [{
-              course_id : data.sbjtId,
-              day: YoilConverter(data.yoil),
-              start: time.start,
-              end: time.end,
-            }, ...times];
-          });
-          actions.addCourses(courses);
-          actions.addTimes(times);
-          this.props.dispatch(saveAppData());
-
-          this.setState({
-            isLoading: false
-          });
-          Alert.alert(
-            '안내', '시간표 불러오기 성공',
-            [
-              {text: 'OK', onPress: () => {
-                this._onPressClose();
-              }}
-            ]
-          );
-        });
-      }
-      else {
-        this.setState({
-          isLoading: false
-        });
-        Alert.alert('안내', '로그인에 실패했습니다.', [{text: 'OK'}]);
-      }
-    })
-    .catch((error) => {
-
-      console.log(error);
-      this.setState({
-        isLoading: false
-      });
-      Alert.alert('안내', '서버 연결에 실패하였습니다.', [{text: 'OK'}]);
-    });
+    this.props.onClickLogin(this.state.id, this.state.password);
   }
 
   render() {
@@ -198,7 +100,7 @@ export default class LoginKunnect extends Component {
             <Text style={styles.messageText}>KUNNECT 회원이 아니시라면 먼저 <Text style={{color:'#666', fontWeight: 'bold'}}>가입</Text>을 해주시기 바랍니다.</Text>
           </View>
         </View>
-        <TouchableOpacity activeOpacity={0.5} style={{position: 'absolute', right: 25, top:25}} onPress={this._onPressClose}>
+        <TouchableOpacity activeOpacity={0.5} style={{position: 'absolute', right: 25, top:25}} onPress={this.props.onClickClose}>
           <Icon name='close' size={35} color='#999' />
         </TouchableOpacity>
       </View>
