049ac7d69b1599acc5680cdf4b6505b3bee910b6
upgle <admin@upgle.com>
2016-07-17 19:59:19 +0900

    refactoring app.js

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import thunkMiddleware from 'redux-thunk';
 import devTools from 'remote-redux-devtools';
-import {Provider, connect} from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import GoogleAnalytics from 'react-native-google-analytics-bridge';
-import {Scene, Router, Actions} from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 
 import timetableApp from '../reducers/timetableApp';
 import StoredTimeTable from './StoredTimeTable';
 import KunnectContainer from './KunnectContainer';
 import AddCourseContainer from './AddCourseContainer';
-import {fetchAppData} from '../actions/appActions';
+import { fetchAppData } from '../actions/appActions';
 
 /**
  * Config Google Analytics
@@ -20,57 +20,57 @@ GoogleAnalytics.trackScreenView('Home');
 
 const RouterWithRedux = connect()(Router);
 const enhancer = compose(
-    applyMiddleware(thunkMiddleware),
-    devTools()
+  applyMiddleware(thunkMiddleware),
+  devTools()
 );
 let store = createStore(timetableApp, enhancer);
 store.dispatch(fetchAppData());
 
-
 // define this based on the styles/dimensions you use
 const getSceneStyle = (/* NavigationSceneRendererProps */ props, computedProps) => {
-    const style = {
-        flex: 1,
-        backgroundColor: '#fff',
-        shadowColor: null,
-        shadowOffset: null,
-        shadowOpacity: null,
-        shadowRadius: null,
-    };
-    if (computedProps.isActive) {
-        style.marginTop = computedProps.hideNavBar ? 0 : 64;
-    }
-    return style;
+  const style = {
+    flex: 1,
+    backgroundColor: '#fff',
+    shadowColor: null,
+    shadowOffset: null,
+    shadowOpacity: null,
+    shadowRadius: null,
+  };
+  if (computedProps.isActive) {
+    style.marginTop = computedProps.hideNavBar ? 0 : 64;
+  }
+  return style;
 };
 
 export default class App extends Component {
 
-    render() {
-        return (
-            <Provider store={store}>
-                <RouterWithRedux getSceneStyle={getSceneStyle}>
-                    <Scene key="root" hideNavBar>
-                        <Scene key="home" component={StoredTimeTable} initial={true} />
-                    </Scene>
-                    <Scene key="kunnect" animation="fade">
-                        <Scene key="kunnect1" component={KunnectContainer} title="쿠넥트 로그인" hideNavBar />
-                    </Scene>
-                    <Scene key="addCourse" animation="fade">
-                        <Scene key="addCourse1"
-                               animation="fade"
-                               hideNavBar={false}
-                               component={AddCourseContainer}
-                               title="강의 추가"
-                               leftTitle="취소"
-                               onLeft={props => Actions.pop()}
-                               navigationBarStyle={{backgroundColor:'#303c4c'}}
-                               titleStyle={{color:'#fff'}}
-                               leftButtonTextStyle={{color:'#ffffff'}}
-                               rightButtonTextStyle={{color:'#ffffff'}}
-                        />
-                    </Scene>
-                </RouterWithRedux>
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <RouterWithRedux getSceneStyle={getSceneStyle}>
+          <Scene key="root" hideNavBar>
+            <Scene key="home" component={StoredTimeTable} initial={true}/>
+          </Scene>
+          <Scene key="kunnect" animation="fade">
+            <Scene key="kunnect1" component={KunnectContainer} title="쿠넥트 로그인" hideNavBar/>
+          </Scene>
+          <Scene key="addCourse" animation="fade">
+            <Scene
+              key="addCourse1"
+              animation="fade"
+              hideNavBar={false}
+              component={AddCourseContainer}
+              title="강의 추가"
+              leftTitle="취소"
+              onLeft={() => Actions.pop()}
+              navigationBarStyle={{ backgroundColor: '#303c4c' }}
+              titleStyle={{ color: '#fff' }}
+              leftButtonTextStyle={{ color: '#ffffff' }}
+              rightButtonTextStyle={{ color: '#ffffff' }}
+            />
+          </Scene>
+        </RouterWithRedux>
+      </Provider>
+    );
+  }
 }
