a87edd3dd76eba02388768178d32aa923252a862
upgle <admin@upgle.com>
2016-07-17 19:57:25 +0900

    refactoring.

--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,6 @@ import {
   AppRegistry
 } from 'react-native';
 
-import App from './app/containers/app';
+import App from './app/containers/App';
 
 AppRegistry.registerComponent('App', () => App);
