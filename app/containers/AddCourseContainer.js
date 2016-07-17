5bcdf9cac9aa4a8b2c378bb28016c550f6a38da7
upgle <admin@upgle.com>
2016-07-17 20:12:34 +0900

    add course container refactoring.

--- app/containers/AddCourseContainer.js
+++ app/containers/AddCourseContainer.js
@@ -1,144 +1,131 @@
+import React, { Component } from 'react';
+import { Alert } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Actions } from 'react-native-router-flux';
 
-import React, {Component} from 'react';
-import * as AppActions from '../actions/appActions';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
 import AddCourse from '../components/AddCourse';
-import {Actions} from 'react-native-router-flux';
-import { saveAppData } from '../actions/appActions';
-import {Alert} from 'react-native';
-
-var uuid = require('uuid');
+import * as AppActions from '../actions/appActions';
+const uuid = require('uuid');
 
 class AddCourseContainer extends Component {
 
-    constructor(props) {
-        super(props);
-        this._onPressDone = this._onPressDone.bind(this);
-        this._onPressDelete = this._onPressDelete.bind(this);
+  constructor(props) {
+    super(props);
+    this.onPressDone = this.onPressDone.bind(this);
+    this.onPressDelete = this.onPressDelete.bind(this);
+  }
+
+  componentWillMount() {
+    Actions.refresh({
+      onRight: this.onPressDone,
+      rightTitle: '완료',
+    });
+  }
+
+  onPressDone() {
+    const component = this.refs.addCourse;
+    const { state, times } = component;
+    const { actions } = this.props;
+
+    component.closeModalAndKeyboard();
+
+    if (this.validation() === false) {
+      return false;
     }
 
-    componentWillMount() {
-        Actions.refresh({
-            onRight: this._onPressDone,
-            rightTitle : "완료"
-        });
+    let courseId = uuid.v4();
+    let cloneTimes = [];
+
+    if (this.props.course_id) {
+      courseId = this.props.course_id;
+      actions.deleteAllTimesByCourseId(courseId);
+    } else {
+      actions.addCourse(courseId, state.subject, state.professor, state.classroom);
     }
 
-    _validation() {
-        const component = this.refs.addCourse;
-        const { state, times } = component;
+    times.forEach((time) => {
+      const data = Object.assign({ course_id: courseId }, time);
+      cloneTimes.push(data);
+    });
+    actions.addTimes(cloneTimes);
 
-        if(times.length === 0) {
-            Alert.alert(
-                '안내', '강의 시간을 한개이상 등록해주시기 바랍니다.',
-                [
-                    {text: '확인'}
-                ]
-            );
-            return false;
-        }
+    this.props.dispatch(AppActions.saveAppData());
+    Actions.pop();
 
-        if(state.subject.length === 0) {
-            Alert.alert(
-                '안내', '강의 이름을 입력해주시기 바랍니다.',
-                [
-                    {text: '확인'}
-                ]
-            );
-            return false;
-        }
+    return true;
+  }
 
-        if(state.professor.length === 0) {
-            Alert.alert(
-                '안내', '교수명을 입력해주시기 바랍니다.',
-                [
-                    {text: '확인'}
-                ]
-            );
-            return false;
-        }
+  onPressDelete() {
+    const { actions, course_id } = this.props;
+    if (course_id) {
+      actions.deleteCourse(course_id);
+      this.props.dispatch(AppActions.saveAppData());
+      Actions.pop();
+    }
+  }
 
-        if(state.classroom.length === 0) {
-            Alert.alert(
-                '안내', '강의실을 입력해주시기 바랍니다.',
-                [
-                    {text: '확인'}
-                ]
-            );
-            return false;
-        }
+  validation() {
+    const component = this.refs.addCourse;
+    const { state, times } = component;
 
-        return true;
+    if (times.length === 0) {
+      this.alert('강의 시간을 한개이상 등록해주시기 바랍니다.');
+      return false;
     }
 
-    _onPressDone() {
-        const component = this.refs.addCourse;
-        const { state, times } = component;
-        const { actions } = this.props;
-
-        component.closeModalAndKeyboard();
-
-        if(this._validation() === false) {
-            return false;
-        }
-
-        var course_id = uuid.v4();
-        var cloneTimes = [];
-
-        if(this.props.course_id) {
-            course_id = this.props.course_id;
-            actions.deleteAllTimesByCourseId(course_id);
-        }
-        else {
-            actions.addCourse(course_id, state.subject, state.professor, state.classroom);
-        }
-
-        times.forEach((time) => {
-            let data = Object.assign({course_id : course_id}, time);
-            cloneTimes.push(data);
-        });
-        actions.addTimes(cloneTimes);
-
-        this.props.dispatch(saveAppData());
-        Actions.pop();
+    if (state.subject.length === 0) {
+      this.alert('강의 이름을 입력해주시기 바랍니다.');
+      return false;
     }
 
-    _onPressDelete() {
-        const { actions, course_id } = this.props;
-        if(course_id) {
-            actions.deleteCourse(course_id);
-            this.props.dispatch(saveAppData());
-            Actions.pop();
-        }
+    if (state.professor.length === 0) {
+      this.alert('교수명을 입력해주시기 바랍니다.');
+      return false;
     }
 
-    render() {
-
-        const { state } = this.props;
-
-        if(this.props.course_id && state.courses[this.props.course_id]) {
-            let times = state.times.filter((time) => time.course_id === this.props.course_id),
-                info = state.courses[this.props.course_id];
-
-            return <AddCourse ref="addCourse"
-                              showDeleteBtn={true}
-                              onPressDelete={this._onPressDelete}
-                              times={times}
-                              subject={info.subject}
-                              professor={info.professor}
-                              classroom={info.classroom} />;
-        }
-        return <AddCourse ref="addCourse"/>;
+    if (state.classroom.length === 0) {
+      this.alert('강의실을 입력해주시기 바랍니다.');
+      return false;
     }
+
+    return true;
+  }
+
+  alert(message) {
+    Alert.alert(
+      '안내', message,
+      [
+        { text: '확인' },
+      ]
+    );
+  }
+
+  render() {
+    const { state } = this.props;
+
+    if (this.props.course_id && state.courses[this.props.course_id]) {
+      let times = state.times.filter((time) => time.course_id === this.props.course_id);
+      let info = state.courses[this.props.course_id];
+
+      return (
+        <AddCourse
+          ref="addCourse"
+          showDeleteBtn={true}
+          onPressDelete={this.onPressDelete}
+          times={times}
+          subject={info.subject}
+          professor={info.professor}
+          classroom={info.classroom} />);
+    }
+    return <AddCourse ref="addCourse"/>;
+  }
 }
 
-export default connect(state => ({
-        state: state
-    }),
-    (dispatch) => ({
-        dispatch: dispatch,
-        actions: bindActionCreators(AppActions, dispatch)
-    })
+export default connect(state =>({
+  state: state,
+}), (dispatch) => ({
+  dispatch: dispatch,
+  actions: bindActionCreators(AppActions, dispatch),
+})
 )(AddCourseContainer);
-
