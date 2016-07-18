dec93d1a71218ab48c23bfb0f5b9285428f3b44e
upgle <admin@upgle.com>
2016-07-18 14:32:12 +0900

    사이드 메뉴 레이아웃 수정.

--- app/components/SideMenu.js
+++ app/components/SideMenu.js
@@ -1,19 +1,9 @@
-
-
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
   Text,
   View,
   Image,
-  ScrollView,
   TouchableHighlight,
-  findNodeHandle,
-  CameraRoll,
-  Dimensions,
-  AsyncStorage,
-  Alert,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -24,28 +14,25 @@ export default class SideMenu extends Component {
   constructor(props) {
     super(props);
 
-    this._onPressAddCourse = this._onPressAddCourse.bind(this);
-    this._onPressLogin = this._onPressLogin.bind(this);
+    this.onPressAddCourse = this.onPressAddCourse.bind(this);
+    this.onPressLogin = this.onPressLogin.bind(this);
   }
 
-  _onPressAddCourse() {
+  onPressAddCourse() {
     Actions.addCourse();
   }
 
-  _onPressLogin() {
+  onPressLogin() {
     Actions.kunnect();
   }
 
   render() {
-
     const { state } = this.props;
 
-    var notiStatus;
-
+    let notiStatus;
     if(state.alarm) {
       notiStatus = (<View style={{position: 'absolute', width: 42, right: 20, top: 14, borderRadius:13, borderColor:'#c9d9f4', borderWidth: 1, paddingLeft: 6, paddingRight: 6, paddingTop: 2, paddingBottom: 2}}><Text style={{color:'#c9d9f4', fontSize: 12, textAlign: 'center'}}>ON</Text></View>);
-    }
-    else {
+    } else {
       notiStatus = (<View style={{position: 'absolute', width: 42, right: 20, top: 14, borderRadius:13, borderColor:'#8f9aad', borderWidth: 1, paddingLeft: 6, paddingRight: 6, paddingTop: 2, paddingBottom: 2}}><Text style={{color:'#8f9aad', fontSize: 12, textAlign: 'center'}}>OFF</Text></View>);
     }
 
@@ -56,36 +43,35 @@ export default class SideMenu extends Component {
             style={styles.icon}
             source={require('../resources/brand.png')}
           />
-          <Text style={styles.brandText}>황소 시간표</Text>
         </View>
-        <TouchableHighlight underlayColor='#273242' onPress={this._onPressAddCourse}>
+        <TouchableHighlight underlayColor='#273242' onPress={this.onPressAddCourse}>
           <View style={styles.menu}>
-            <Icon name='add' color='#a1acc1' size={22} />
+            <Icon name='add' color='#afbcd4' size={25} style={{ width: 32 }} />
             <Text style={styles.menuText}>강의 추가</Text>
           </View>
         </TouchableHighlight>
-        <TouchableHighlight underlayColor='#273242' onPress={this._onPressLogin}>
+        <TouchableHighlight underlayColor='#273242' onPress={this.onPressLogin}>
           <View style={styles.menu}>
-            <Icon name='get-app' color='#a1acc1' size={22} />
+            <Icon name='get-app' color='#afbcd4' size={22} style={{ width: 32 }} />
             <Text style={styles.menuText}>시간표 불러오기</Text>
           </View>
         </TouchableHighlight>
         <TouchableHighlight underlayColor='#273242' onPress={this.props.onPressSaveTimetable}>
           <View style={styles.menu}>
-            <Icon name='collections' color='#a1acc1' size={20} />
+            <Icon name='collections' color='#afbcd4' size={20} style={{ width: 32 }} />
             <Text style={styles.menuText}>앨범에 저장</Text>
           </View>
         </TouchableHighlight>
         <TouchableHighlight underlayColor='#273242' onPress={this.props.onPressHeaderColorset}>
           <View style={styles.menu}>
-            <Icon name='color-lens' color='#a1acc1' size={20} />
+            <Icon name='color-lens' color='#afbcd4' size={20} style={{ width: 32 }} />
             <Text style={styles.menuText}>컬러셋</Text>
             <View style={{position: 'absolute', top: 19, right:20, borderRadius:12, width: 12, height: 12, backgroundColor:this.props.themeColor}}></View>
           </View>
         </TouchableHighlight>
         <TouchableHighlight underlayColor='#273242' onPress={this.props.onPressAlarm}>
           <View style={styles.menu}>
-            <Icon name='alarm' color='#a1acc1' size={20} />
+            <Icon name='alarm' color='#afbcd4' size={20} style={{ width: 32 }} />
             <Text style={styles.menuText}>수업 알림</Text>
             {notiStatus}
           </View>
@@ -98,9 +84,9 @@ export default class SideMenu extends Component {
 const styles = {
   container: {
     flex: 1,
-    backgroundColor:'#303c4c'
+    backgroundColor:'#303c4c',
   },
-  brand : {
+  brand: {
     height: 140,
     borderBottomColor: '#232c3a',
     borderBottomWidth: 1,
@@ -112,17 +98,17 @@ const styles = {
     marginTop: 14,
     color: '#818b9c',
   },
-  menu : {
+  menu: {
     flexDirection : 'row',
     height: 50,
     paddingLeft: 20,
     borderBottomColor: '#232c3a',
     borderBottomWidth: 1,
     alignSelf: 'stretch',
-    alignItems: 'center'
+    alignItems: 'center',
   },
-  menuText : {
-    marginLeft: 10,
-    color: '#a1acc1'
-  }
+  menuText: {
+    color: '#afbcd4',
+    fontSize: 13.5,
+  },
 };
