13562183e90f9bd678739b61eb1a3f6540a543e0
upgle <admin@upgle.com>
2016-07-24 17:13:59 +0900

    kakao sdk 추가.

---
+++ ios/App/KakaoManager.m
@@ -0,0 +1,13 @@
+//
+//  KakaoManager.m
+//  App
+//
+//  Created by seong-hyunoh on 2016. 7. 24..
+//  Copyright © 2016년 Facebook. All rights reserved.
+//
+
+#import "KakaoManager.h"
+
+@implementation KakaoManager
+
+@end
